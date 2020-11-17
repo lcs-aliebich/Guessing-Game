6ba8cd55ba828fee57a057da4347c5f245656abc
Andreas Liebich <aliebich@lcs.on.ca>
2020-11-17 15:09:00 -0500

    stuff

--- Guessing Game/ContentView.swift
+++ Guessing Game/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var theUserGuess = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+     
+        NavigationView {
+            VStack {
+                
+                Text("Guessing Game")
+                Text ("Im thinking of a number between 1 and 100 guess what it is.")
+                
+                TextField("enter you guess here",
+                          text: $theUserGuess)
+                
+                Button("Submit Guess") {
+                    // Check the guess
+                }
+                Text("You guessed \(theUserGuess).")
+                
+            }
+            .navigationTitle("Guessing Game") 
     }
 }
 
@@ -19,3 +36,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
