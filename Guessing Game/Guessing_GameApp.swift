b023800eda6964fc1c420eaea20f5dfd381e6670
Andreas Liebich <aliebich@lcs.on.ca>
2020-11-17 14:40:15 -0500

    Initial Commit

---
+++ Guessing Game/Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_GameApp.swift
+//  Guessing Game
+//
+//  Created by Andreas Liebich on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
